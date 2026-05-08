b2b5469930bf9aed1ef1a5a4588d9d87dd8a4036
biscuitrescue <karttikeyasinha11@gmail.com>
2026-05-09 02:29:03 +0530

    added get_songs

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
+#include <dirent.h>
+#include <linux/limits.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
-#include <linux/limits.h>
-#include <dirent.h>
+#include <unistd.h>
 
 typedef struct {
   char **items;
@@ -15,24 +15,33 @@ typedef struct {
 } SongList;
 
 bool is_audio(const char *path);
+void push_song(SongList *songs, const char *path);
+void scan_dir(SongList *songs, const char *path);
 
-void read_library(SongList *songs, const char *cwd) {
-
-  DIR *dir;
-  struct dirent *entry;
-  dir = opendir(cwd);
+void scan_dir(SongList *songs, const char *path) {
+  DIR *dir = opendir(path);
 
   if (dir == NULL) {
     perror("Couldn't open directory");
     exit(EXIT_FAILURE);
   }
-
+  
+  struct dirent *entry;
+  char full_path[PATH_MAX];
+  
   while ((entry = readdir(dir)) != NULL) {
-    if (is_audio(entry->d_name)) {
-      printf("%s\n", entry->d_name);
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+      continue;
+    }
+    
+    snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+    
+    if (entry->d_type == DT_DIR) {
+      scan_dir(songs, full_path);
+    } else if (is_audio(entry->d_name)) {
+      push_song(songs, full_path);
     }
   }
-
   closedir(dir);
 }
 
@@ -46,30 +55,41 @@ void push_song(SongList *songs, const char *path) {
   songs->items[songs->len] = strdup(path);
   songs->len++;
 }
-
+ 
 bool is_audio(const char *path) {
-  char *ext = (strrchr(path, '.'));
+  const char *ext = (strrchr(path, '.'));
 
   if (!ext || ext[1] == '\0') {
     return false;
   }
-
   ext++;
-  
   return !strcasecmp(ext, "mp3") || !strcasecmp(ext, "flac") ||
          !strcasecmp(ext, "ogg") || !strcasecmp(ext, "wav");
 }
 
-int main(int argc, char *argv[]) {
-  char cwd[PATH_MAX];
+void get_songs(SongList *songs) {
+  if (songs->len == 0) {
+    printf("Nothing in directory, exiting...");
+    exit(EXIT_SUCCESS);
+  }
+  for (size_t i = 0; i < songs->len; i++) {
+    printf("%s\n", songs->items[i]);
+    /* printf("%zu\n", i); */
+  }
+}
 
-  if (argc == 0) {
-    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+int main(int argc, char *argv[]) {
+  char buf[PATH_MAX];
+  char *path;
+
+  if (argc < 2) {
+    if (getcwd(buf, sizeof(buf)) == NULL) {
       perror("Couldn't set cwd");
       exit(EXIT_FAILURE);
     }
+    path = buf;
   } else {
-    const char *cwd = ".";
+    path = argv[1];
   }
 
   SongList songs;
@@ -78,7 +98,8 @@ int main(int argc, char *argv[]) {
 
   songs.items = malloc(sizeof(char *) * songs.cap);
 
-  read_library(&songs, cwd);
+  scan_dir(&songs, path);
+  get_songs(&songs);
 
   for (size_t i = 0; i < songs.len; i++) {
     free(songs.items[i]);
